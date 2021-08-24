64125c1facd77f19e01c34dc51ca710f4c75ac74
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-08-24 18:41:59 +0700

    how python executes function

--- functions.py
+++ functions.py
@@ -1,1 +1,8 @@
 # FUNCTIONS ARE VALUE and can be assigned to variable
+# HOW PYTHON EXECUTES FUNCTION:
+    # 1 the function is CALLED and any arguments are passed to the function as input
+    # 2 The function executes and some actions are performed with the arguments
+    # 3 The function returns and the original function call is replaces with return value.
+
+num_letters = len("four")
+print(num_letters)
