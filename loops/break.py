9b68e4d26ccc8ffa9d75b1a47183b720bee9f983
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-08-28 00:50:04 +0700

    add example of the use of break statement

--- loops/break.py
+++ loops/break.py
@@ -1,1 +1,8 @@
 # The Python break statement immediately terminates a loop entirely. Program execution proceeds to the first statement following the loop body.
+n = 5
+while n > 0:
+    n -= 1
+    if n == 2:
+        break
+    print(n)
+print("Loop ended!")
