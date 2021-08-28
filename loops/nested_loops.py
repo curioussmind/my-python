bea0516c3bf8362175822419a91d9c8e99d0b24f
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-08-28 07:15:53 +0700

    update nested_loops.py

--- loops/nested_loops.py
+++ loops/nested_loops.py
@@ -3,3 +3,10 @@
 for n in range(1, 4):
     for j in range(4, 7):
         print(f"n = {n} and j = {j}")
+
+a = ["foo", 'bar']
+while len(a):
+    print(a.pop(0))
+    b = ['baz', 'qux']
+    while len(b):
+        print('>', b.pop(0))
