b9744abb2ac0963280fb7309058f49cc327f10d8
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-09-04 23:25:46 +0700

    update tuples.py

--- tuples_lists_dicts/tuples.py
+++ tuples_lists_dicts/tuples.py
@@ -1,1 +1,16 @@
-# tuple is 
+# tuple is a collection of item separated by comma in a parenthesis.
+my_tupple = (1, 2, 3) # tuple with three items
+
+empty_tupple = () # empty tuple
+
+#  THE TUPLE BUILT IN
+name = "oktavianus"
+my_tupple = tuple(name)
+print(my_tupple)
+print(len(my_tupple)) # print length of tuple
+print(my_tupple[1]) # tuple indexing
+print(my_tupple[2:4]) # slicing
+
+# tuple are immutable means we can't change the value of an element in the tuple
+
+print("n" in my_tupple) # checking value existence
