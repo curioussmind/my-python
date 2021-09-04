2355f9236324a1ba67106073f2e033931ae6de9b
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-09-04 23:49:18 +0700

    add cardinal number.py

---
+++ tuples_lists_dicts/cardinal_numbers.py
@@ -0,0 +1,10 @@
+cardinal_numbers = ("first", "second", "third") # creating tuple literal
+print(type(cardinal_numbers)) # creating tuple literal
+print(cardinal_numbers[1]) # print element at index 1
+position_1, position_2, position_3 = cardinal_numbers # tuple unpacking
+print(position_1, position_2, position_3)
+
+my_name = ("o", "k", "t", "a", "v", "i", "a", "n", "u", "s")
+print(type(my_name))
+print("x" in my_name)
+print(my_name[1:])
