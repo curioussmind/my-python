8ab69f0f2177b3d140d54e9680b4e9720d2e3e8a
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-09-05 23:36:16 +0700

    add list.py

---
+++ tuples_lists_dicts/list.py
@@ -0,0 +1,2 @@
+# lists are mutable and indexed by int starting from 0
+# list and tuple are quiet similar but different, the obvious different is that tuple uses paranthesis but list uses square brackets
