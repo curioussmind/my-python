488ddf7d231a079c60a4ce1d3be536ba94ce5c30
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-09-07 23:10:41 +0700

    updating list.py

--- tuples_lists_dicts/list.py
+++ tuples_lists_dicts/list.py
@@ -1,2 +1,16 @@
 # lists are mutable and indexed by int starting from 0
 # list and tuple are quiet similar but different, the obvious different is that tuple uses paranthesis but list uses square brackets
+
+colors = ["red", "blue", "yellow", "green"] # creatinng list literal
+
+list((1, 2, 3)) # creating list using list() built in function.
+
+list("python") # creating list from string
+
+groceries = "egges, milk, cheese"
+grocery_list = groceries.split(", ") # creating list using split() method
+
+"a;b;c".split(";") # >> ['a', 'b', 'c']
+
+
+
