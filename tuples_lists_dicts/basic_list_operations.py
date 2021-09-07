41f7e86f75c8755e446bdad50ffc2a10276bcd27
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-09-08 00:52:40 +0700

    updating basic_list_operation.py

--- tuples_lists_dicts/basic_list_operations.py
+++ tuples_lists_dicts/basic_list_operations.py
@@ -15,3 +15,22 @@ colors = ["red", "yellow", "green", "blue"]
 colors[0] = "burgundy" 
 colors # >> ["burgundy", "yellow", "green", "blue"]
 
+# LIST METHOD FOR ADDING AND REMOVING ELEMENT
+# insert() used to add a single value into a list. It takes two parameters, the index and the value
+colors.insert(1, "orange") 
+# if the value for index parameter .insert() is larger than the greatest index in the list, 
+# the value will be inserted at the end.
+colors.insert(20, 'violet')
+colors.insert(-1, "indigo") # we can also use negative indices
+
+# REMOVE ELEMENT OF AT A SPECIFIC INDEX
+colors = colors.pop(3) # remove the value from index 3
+# colors.pop(-1) # this also works
+
+colors.append("purple") # add new element at the end of a list
+
+colors.insert(len(colors), "black") # insert black at the end of a list
+
+# add several items/ elements to a list
+colors.extend(['white', 'maroon'])
+print(colors)
