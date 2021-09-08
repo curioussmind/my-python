2177693c9ea79def325ab08325270ae71ad8b3fb
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-09-08 21:27:19 +0700

    add sorting_list.py

---
+++ tuples_lists_dicts/sorting_list.py
@@ -0,0 +1,21 @@
+# list have sort method to sort all items in ascending orders
+colors = ["red", "yellow", "green", "blue"]
+colors.sort()
+print(colors)
+
+# sorted numeric items
+numbers = [1, 10, 5, 3, 2]
+numbers.sort()
+print(numbers)
+
+my_friend_name = ["dardi", "abot", "jujus", "hendro"]
+my_friend_name.sort(key=len) # sort the items based on the length of each element
+print(my_friend_name)
+
+# we can also pass user defined function to key
+def get_second_element(item):
+    return item[1]
+
+items = [(4, 1), (1, 2), (-9, 0)]
+items.sort(key=get_second_element)
+print(items)
