72b614462a9f7696bfec481cd4d1c3a9b732bb41
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-09-09 00:53:21 +0700

    update exercises_2.py

--- tuples_lists_dicts/exercises_2.py
+++ tuples_lists_dicts/exercises_2.py
@@ -4,3 +4,9 @@ for n in data:
     total = n[0] + n[1]
     row += 1
     print(f"Row {row} sum: {total}")
+
+numbers = [4, 3, 2, 1]
+numbers_copy = numbers[:]
+numbers.sort()
+print(numbers)
+print(numbers_copy)
