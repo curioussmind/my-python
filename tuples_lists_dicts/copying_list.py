208c4feb110f8c237ab0791aa6dc9ca260718fe0
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-09-08 21:17:29 +0700

    update copying_list.py

--- tuples_lists_dicts/copying_list.py
+++ tuples_lists_dicts/copying_list.py
@@ -3,3 +3,12 @@ animals = ["lion", "tiger", "frumious Bandersnatch"]
 large_cats = animals
 large_cats.append("Tiger")
 print(animals)
+
+# animals and large_cats now refer to the same list. how can we have different list?
+#USING SLICING approach
+
+large_catss = animals
+large_catss = animals[:]
+large_catss.append("leopard")
+print(large_catss)
+print(animals)
