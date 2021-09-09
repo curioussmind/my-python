e3cd890b70d65a42a3614553fda65347a9119813
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-09-09 19:49:15 +0700

    add list_of_list.py

--- list_of_lists.py
+++ list_of_lists.py
@@ -11,6 +11,28 @@ def enrollment_stats(list_of_universities):
     # return variables
     return total_students, total_tuition
 
+def mean(values):
+    """return the man values of each list."""
+    return sum(values) / len(values)
+
+def median(values):
+    """return the median values of each list."""
+    values.sort()
+    # if the number if values is odd
+    # return the middle value of the list
+    if len(values) % 2 == 1:
+        # The value at the center of the list is the value
+        # at whose index is half of the length of the list,
+        # rounded down
+        center_index = int(len(values) / 2)
+        return values[center_index]
+    # Otherwise, if the length of the list is even, return
+    # the mean of the two center values
+    else:
+        left_center_index = (len(values) - 1) // 2
+        right_center_index = (len(values) + 1) // 2
+        return mean([values[left_center_index], values[right_center_index]])
+
 universities = [
     ['California Institute of Technology', 2175, 37704],
     ['Harvard', 19627, 39849],
@@ -21,5 +43,15 @@ universities = [
     ['Yale', 11701, 40500]
 ]
 
+totals = enrollment_stats(universities)
 
-    
+print("\n")
+print("*****" * 6)
+print(f"total students:     {sum(totals[0]):,}")
+print(f"total tuitions:     $ {sum(totals[1]):},")
+print(f"\nStudent mean:    {mean(totals[0]):,.2f}")
+print(f"Student median:    {median(totals[0]):,}")
+print(f"\nTuition mean:    {mean(totals[1]):,.2f}")
+print(f"Tuition median:    {median(totals[1]):,.2f}")
+print("*****" * 6)
+print("\n")
