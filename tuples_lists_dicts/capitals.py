dee42f2a512d0d08d4c5cd21af67369bb3d5c3d4
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-09-11 17:51:53 +0700

    add capitals.py

---
+++ tuples_lists_dicts/capitals.py
@@ -0,0 +1,15 @@
+import random
+
+capital_dict = {
+    'Alabama': 'Montgomery',
+    'Alaska': 'Juneau',
+    'Arizona': 'Phoenix',
+    'Arkansas': 'Little Rock',
+    'California': 'Sacramento',
+    'Colorado': 'Denver',
+    'Connecticut': 'Hartford',
+    'Delaware': 'Denver',
+    'Florida': 'Tallahassee',
+    'Georgia': 'Atlanta',
+}
+
