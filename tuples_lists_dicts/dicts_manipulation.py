30bb56616c9a633b1fe9007913ba69c6fa4110d0
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-09-11 13:47:48 +0700

    change accessing_dict to dicts_manipulation

---
+++ tuples_lists_dicts/dicts_manipulation.py
@@ -0,0 +1,9 @@
+capitals = {
+    "California": "Sacramento", #each key and value separated by colon (:)
+    "New York": "Albany", # each items or key and value pairs separated by comma
+    "Texas": "Austin", # enclosed by curly braces
+}
+
+print(capitals["Texas"]) # items in dictionary are accessed by key
+
+# adding and removing value
