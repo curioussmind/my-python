3c26ca0abf4722b9839142ec5f9d6874700c3fea
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-09-22 15:03:34 +0700

    working on packages exercises

---
+++ modules_packages/packages_example/package_exercises/helpers/string.py
@@ -0,0 +1,5 @@
+# string.py
+
+def shout(hey):
+    return hey.upper()
+    
---
+++ modules_packages/packages_example/package_exercises/main.py
@@ -0,0 +1,8 @@
+from modules_packages.packages_example.package_exercises.helpers.math import area
+from modules_packages.packages_example.package_exercises.helpers.string import shout
+import helpers.string
+import helpers.math
+
+
+area(5, 8)
+shout()
