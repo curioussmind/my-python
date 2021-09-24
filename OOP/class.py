0fc519379e0d22e66016a0dba87655edabfcebf8
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-09-25 06:02:50 +0700

    modify class.py according to the exercise

--- OOP/class.py
+++ OOP/class.py
@@ -1,9 +1,34 @@
+from typing import Mapping
+
+
 class Dog:
     species = "Canis familiaris"
-    def __init__(self, name, age):
+
+    def __init__(self, name, age, coat_color):
         self.name = name
         self.age = age
+        self.coat_color = coat_color
 
-buddy = Dog("Buddy", 9)
-miles = Dog("Miles", 4)
+    # instance method
+    def __str__(self):
+        return f"{self.name} is {self.age} years old."
+    
+    # instance method
+    def speak(self, sound):
+        return f"{self.name} says {sound}."
+    
+
+#miles = Dog("Miles", 4) # creating object
+#print(miles)
+
+#sound_woof = miles.speak("Woof Woof") # calling instance method
+#print(sound_woof)
+
+#sound_bow = miles.speak("Bow Bow") # calling instance method
+#print(sound_bow)
+
+phillo = Dog("Phillo", 5, "brown")
+print(f"{phillo.name}'s coat is {phillo.coat_color}.")
+
+
 
