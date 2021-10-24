f80918c899bb4361a879752ddf3719f45aa2d4f4
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-10-24 15:17:57 +0700

    update common_filesystem_operations.py

---
+++ file_input_output/common_filesystem_operations.py
@@ -0,0 +1,20 @@
+# GOALS
+    # create dir and files
+    # search for files within dir
+    # move and delete files and folders
+
+# CREATING DIRS AND FILES
+from pathlib import Path
+new_dir = Path.home() / "new_dir"
+new_dir.mkdir()
+print(new_dir.exists()) # checking whether the dir is exist or not
+
+# we can do this if we want to create new dir and want to avoid the fileexisterror
+new_dir.mkdir(exist_ok=True) # when we run this, it creates the folder it it doesn't exist or nothing happened if it is exist
+
+# to create any parent dirs needed in order to create the target dir, we can set the opt parents parameter of mkdir() to True
+nested_dir = new_dir / "folder_a" / "folder_b" # create folder b inside folder a insied new_dir
+nested_dir.mkdir(parents=True) # since folder a is not exist yet, we set parent to True so that it'll automatically create folder a
+nested_dir.mkdir(parents=True, exist_ok=True) # by putting all if these together, no error will be raised
+
+
