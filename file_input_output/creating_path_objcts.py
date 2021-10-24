e848953eed9ed65a924b1620cffe81721a4232ac
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-10-24 15:28:23 +0700

    update creating file objcts

--- file_input_output/creating_path_objcts.py
+++ file_input_output/creating_path_objcts.py
@@ -2,7 +2,7 @@ from os import pathconf
 import pathlib
 
 # CREATING PATH OBJECT FROM A STRING
-path = pathlib.Path("/Users/macbook/Documents/Diabetes.pdf")
+path = pathlib.Path("Documents/Diabetes.pdf")
 
 # WE CAN ALSO USE A CLASS METHOD THAT RETURN PATH OBJ OF SPECIEAL DIR
 # TWO OF THEM ARE Path.home() and Path.cwd()
