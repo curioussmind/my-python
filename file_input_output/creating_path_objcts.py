eb5bb875939bc7f3dbb4ba0d21f5448129ab9d76
oktavianus misro <oktavianusmisroadrianto@gmail.com>
2021-10-24 14:23:15 +0700

    update creating_path_objcts.py

---
+++ file_input_output/creating_path_objcts.py
@@ -0,0 +1,18 @@
+from os import pathconf
+import pathlib
+
+# CREATING PATH OBJECT FROM A STRING
+path = pathlib.Path("/Users/macbook/Documents/Diabetes.pdf")
+
+# WE CAN ALSO USE A CLASS METHOD THAT RETURN PATH OBJ OF SPECIEAL DIR
+# TWO OF THEM ARE Path.home() and Path.cwd()
+path = 'C:\Users<username' # windows
+path = '/Users/<username>' # macOS
+path = '/home/<username' # Ubuntu linux
+
+# ABSOLUTE VS RELATIVE PATH
+windows = pathlib.Path(r"Photos/image.jpg") #relative
+macOs = pathlib.Path("Photos/image.jpg") # relatif
+
+
+
